917d5f0c656954bff40b21e0f7d8805312efe9f6
V-Gnana-Teja <151600407+V-Gnana-Teja@users.noreply.github.com>
2024-03-20 22:50:00 +0530

    Created Testimonials section

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,4 +1,12 @@
 import React from 'react'
+import Axon from '../Assets/images/Axon.png'
+import Jetstar from '../Assets/images/Jetstar.png'
+import Expedia from '../Assets/images/Expedia.png'
+import Qantas from "../Assets/images/Qantas.png"
+import Alitalia from  '../Assets/images/Alitalia.png'
+import Travello from '../Assets/images/Group 590.png'
+/* import  Mail  from "../Assets/images/mail-5899 (1).png";
+ */
 
 const Testimonials = () => {
   return (
@@ -11,10 +19,65 @@ const Testimonials = () => {
           </h1>
         </div>
 
-        
+          <div>
+            <h1 className='card'> "on the windows talking painted pasture yet its express parties use. Sure last upon he same sa knew next. Of believed or diverted no."
 
-        </div>
+            Teja
+
+            Bengaluru, India
+
+            
+            
+            </h1>
+          </div>
+
+          <div className='flex flex-row justify-between items-center'>
+            <img src={Axon} width={100} height={70} alt="/" />
+            <img src={Jetstar} width={100} height={70} alt="/" />
+            <img src={Expedia} width={250} height={200} alt="/" />
+            <img src={Qantas} width={100} height={70} alt="/" />
+            <img src={Alitalia} width={100} height={70} alt="/" />
+            
+
+          </div>
+
+
+          <div>
+            <p> Subscribe to get information, latest news and other and also to be able to greet the  interesting  offers about Jadoo </p>
+
+            <input type="text"   placeholder =' Your email'/>
+            
+            <button className='bg-red-400 w-7 h-3'>Subscribe</button>
+          </div>
+
+            <footer>
+              <div className='mr-12 mt-12'>
+                <img src={Travello} alt="/" />
+              </div>
+              <ul>
+                <li><h1>Company</h1></li>
+                <li>About</li>
+                <li>Careers</li>
+                <li>Mobile</li>
+              </ul>
+              <ul>
+                <li><h1>Contact</h1></li>
+                <li>Help/FAQ</li>
+                <li>Press</li>
+                <li>Affiliates</li>
+              </ul>
+              <ul>
+                <li><h1>More</h1></li>
+                <li>Airline fees</li>
+                <li>Airline</li>
+                <li>Low fare tips</li>
+              </ul>
+
+              <p>All rights reserved @jadoo.co</p>
+            </footer>
+            </div>
     </div>
+    
   )
 }   
 
